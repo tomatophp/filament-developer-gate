e5626688b4106aad4149c96438909d118696a194
Fady Mondy <info@3x1.io>
2024-04-06 18:03:02 +0200

    first commit 🔥

---
+++ src/FilamentDeveloperGateServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace TomatoPHP\FilamentDeveloperGate;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class FilamentDeveloperGateServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //Register generate command
+        $this->commands([
+           \TomatoPHP\FilamentDeveloperGate\Console\FilamentDeveloperGateInstall::class,
+        ]);
+
+        //Register Config file
+        $this->mergeConfigFrom(__DIR__.'/../config/filament-developer-gate.php', 'filament-developer-gate');
+
+        //Publish Config
+        $this->publishes([
+           __DIR__.'/../config/filament-developer-gate.php' => config_path('filament-developer-gate.php'),
+        ], 'filament-developer-gate-config');
+
+        //Register Migrations
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        //Publish Migrations
+        $this->publishes([
+           __DIR__.'/../database/migrations' => database_path('migrations'),
+        ], 'filament-developer-gate-migrations');
+        //Register views
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'filament-developer-gate');
+
+        //Publish Views
+        $this->publishes([
+           __DIR__.'/../resources/views' => resource_path('views/vendor/filament-developer-gate'),
+        ], 'filament-developer-gate-views');
+
+        //Register Langs
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'filament-developer-gate');
+
+        //Publish Lang
+        $this->publishes([
+           __DIR__.'/../resources/lang' => base_path('lang/vendor/filament-developer-gate'),
+        ], 'filament-developer-gate-lang');
+
+        //Register Routes
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+
+    }
+
+    public function boot(): void
+    {
+        //you boot methods here
+    }
+}
