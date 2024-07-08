4cec435f36cef2a06871cbe7d7cd7b8b11abc7ac
Fady Mondy <info@3x1.io>
2024-07-08 13:18:17 +0300

    [Feature] make dynamic routes from config #1

--- config/filament-developer-gate.php
+++ config/filament-developer-gate.php
@@ -3,5 +3,7 @@
 return [
     "password" => env("DEVELOPER_GATE_PASSWORD", "password"),
 
-    "redirect" => env("DEVELOPER_GATE_REDIRECT", "/admin")
+    "redirect" => env("DEVELOPER_GATE_REDIRECT", "/admin"),
+
+    "route_prefix" => "admin"
 ];
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('web')->group(function (){
-    Route::post('/admin/{tenent}/developer-gate', [TomatoPHP\FilamentDeveloperGate\Http\Controllers\DeveloperGateController::class, 'login'])->name('developer-gate.login.tenent');
-    Route::post('/admin/developer-gate', [TomatoPHP\FilamentDeveloperGate\Http\Controllers\DeveloperGateController::class, 'login'])->name('developer-gate.login');
+Route::middleware('web')->prefix(config('filament-developer-gate.route_prefix'))->group(function (){
+    Route::post('/{tenent}/developer-gate', [TomatoPHP\FilamentDeveloperGate\Http\Controllers\DeveloperGateController::class, 'login'])->name('developer-gate.login.tenent');
+    Route::post('/developer-gate', [TomatoPHP\FilamentDeveloperGate\Http\Controllers\DeveloperGateController::class, 'login'])->name('developer-gate.login');
 });
